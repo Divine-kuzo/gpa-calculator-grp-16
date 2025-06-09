3acd2886f840499bf6362bd550a7461ac33f5ec3
Divine-kuzo <d.kuzo@alustudent.com>
2025-06-09 21:43:22 +0200

    Create script.js

---
+++ script.js
@@ -0,0 +1,6 @@
+// Get elements from the page
+const form = document.getElementById('entry-form');
+const nameInput = document.getElementById('assignment-name');
+const gradeInput = document.getElementById('assignment-grade');
+const gpaDisplay = document.getElementById('gpa-value');
+const entriesList = document.getElementById('entries-list');
